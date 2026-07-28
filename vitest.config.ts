16b93af8b9e76458b6f9fcd4f854bbb41932d385
Floryan Simar <floryansimar@gmail.com>
2026-07-28 16:41:23 +0200

    fix: unable to run vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import process from 'node:process';
-import type { TestProjectConfiguration, ViteUserConfig } from 'vitest/config';
+import type { ViteUserConfig } from 'vitest/config';
 import { defineConfig } from 'vitest/config';
 import pkg from './package.json' with { type: 'json' };
 
@@ -14,18 +14,16 @@ const coverage = {
   reportOnFailure: true,
 } as const satisfies (ViteUserConfig['test'] & {})['coverage'];
 
-export const projects = [
-  {
-    resolve: { tsconfigPaths: true },
-    test: {
-      exclude: [`${import.meta.dirname}/src/**/*.*.{test,spec}.{ts,tsx}`],
-      include: [`${import.meta.dirname}/{src,scripts}/**/*.{test,spec}.{ts,tsx}`],
-      includeSource: [`${import.meta.dirname}/{src,scripts}/**/*.{ts,tsx}`],
-      name: `${name} - unit`,
-      setupFiles: [`${import.meta.dirname}/test/custom-matchers.ts`],
-      tags: [{ description: 'All the unit test', name: 'unit' }],
-    },
+export default defineConfig({
+  resolve: { tsconfigPaths: true },
+  test: {
+    coverage,
+    hookTimeout,
+    include: [`${import.meta.dirname}/{src}/**/*.{test,spec}.{ts,tsx}`],
+    includeSource: [`${import.meta.dirname}/{src,scripts}/**/*.{ts,tsx}`],
+    name: `${name} - unit`,
+    setupFiles: [`${import.meta.dirname}/test/custom-matchers.ts`],
+    tags: [{ description: 'All the unit test', name: 'unit' }],
+    testTimeout,
   },
-] as const satisfies Array<TestProjectConfiguration>;
-
-export default defineConfig({ resolve: { tsconfigPaths: true }, test: { coverage, hookTimeout, projects, testTimeout } });
+});
